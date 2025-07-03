61e3d35baaf545d31fb17944bf794fc373357cdc
bendy41 <sleepdeath315@gmail.com>
2025-07-03 19:03:51 +0900

    Error fixed

--- src/api/teamApi.ts
+++ src/api/teamApi.ts
@@ -4,7 +4,7 @@ import api from './apiClient';
 // 초대코드를 사용해 팀에 가입하는 함수
 export async function joinTeamByCode(code: string): Promise<void> {
   try{
-    await api.post('/api/invite/join', { code });
+    await api.post('/api/invitecode/join',{ inviteCode: code});
     // 성공 시 특별한 리턴 없음
   }catch (error: any) {
     console.error('팀 가입 오류:', error);
