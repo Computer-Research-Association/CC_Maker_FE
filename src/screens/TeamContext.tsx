388b1368aa9754f646ea11f83c75f3fb5fda1d86
bendy41 <sleepdeath315@gmail.com>
2025-07-11 17:30:24 +0900

    Connect Subgroup name

--- src/screens/TeamContext.tsx
+++ src/screens/TeamContext.tsx
@@ -1,10 +1,12 @@
 import React, { createContext, useState, ReactNode } from "react";
 
 interface TeamContextType {
-   teamId: string | null;
+  teamId: string | null;
   setTeamId: React.Dispatch<React.SetStateAction<string | null>>;
-  role: string | null;  // 예: "LEADER", "MEMBER"
+  role: string | null;
   setRole: React.Dispatch<React.SetStateAction<string | null>>;
+  userName: string | null;
+  setUserName: React.Dispatch<React.SetStateAction<string | null>>;
 }
 
 export const TeamContext = createContext<TeamContextType>({
@@ -12,14 +14,18 @@ export const TeamContext = createContext<TeamContextType>({
     setTeamId: () => {},
     role: null,
     setRole: () => {},
+    userName: null,
+    setUserName: () => {},
 });
 
 export const TeamProvider = ({ children }: { children: ReactNode }) => {
   const [teamId, setTeamId] = useState<string | null>(null);
   const [role, setRole] = useState<string | null>(null);
+  const [userName, setUserName] = useState<string | null>(null);
 
   return (
-    <TeamContext.Provider value={{ teamId, setTeamId, role, setRole }}>
+    <TeamContext.Provider value={{ teamId, setTeamId, role, setRole,userName,
+        setUserName,}}>
       {children}
     </TeamContext.Provider>
   );
